4bbc85fe1e2933a9aef0924fee8067212e3c35d5
Rafael Abensur <abensur7@gmail.com>
2017-06-12 13:25:03 -0300

    add example.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,40 @@
 <template>
-  <div id="app" />
+  <div id="app">
+
+    <!--<form>
+      Buscar: <input v-model="searchQuery">
+    </form>
+    <exemplo
+      :data="gridData"
+      :columns="gridColumns"
+      :filter-key="searchQuery">
+    </exemplo>-->
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
 import { mapActions } from 'vuex'
 
+import Exemplo from '@/components/Exemplo'
+
 export default {
   name: 'app',
+  components: {
+    Exemplo
+  },
+  data () {
+    return {
+      searchQuery: '',
+      gridColumns: ['name', 'power'],
+      gridData: [
+        { name: 'Chuck Norris', power: Infinity },
+        { name: 'Bruce Lee', power: 9000 },
+        { name: 'Jackie Chan', power: 7000 },
+        { name: 'Jet Li', power: 8000 }
+      ]
+    }
+  },
   methods: {
     ...mapActions([
       'getPlans',
@@ -26,3 +53,18 @@ export default {
   }
 }
 </script>
+<style scoped>
+#app {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  padding-top: 60px;
+  font-family: 'Helvetica';
+}
+
+#app form {
+  flex: 0 100%;
+  text-align: center;
+  margin-bottom: 30px;
+}
+</style>
