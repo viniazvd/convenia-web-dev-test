7267a0b285c6b517dc28aeab7b69d9e2081c932e
Vinicius Azevedo <viniazvd@gmail.com>
2018-01-15 16:00:29 -0200

    moved

--- src/store/mutations-types.js
+++
@@ -1,3 +0,0 @@
-export const SET_DETAILS = 'auth/SET_DETAILS'
-export const SET_PLANS = 'auth/SET_PLANS'
-export const SET_PRICES = 'auth/SET_PRICES'
--- src/store/state.js
+++
@@ -1,5 +0,0 @@
-export default {
-  plans: [],
-  prices: [],
-  details: []
-}
