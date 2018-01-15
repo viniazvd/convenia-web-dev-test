8b9c18cd8ff4e82e2f5b1d69b58e3c5c2ce18356
Vinicius Azevedo <viniazvd@gmail.com>
2018-01-15 00:31:03 -0200

    nothing relevant

--- src/TheRoot.vue
+++ src/TheRoot.vue
@@ -4,7 +4,6 @@
       Segundo o style-guide oficial do vue, os componentes que só devem ter uma única
      instância ativa devem começar com o Theprefixo, para denotar que pode haver apenas um. 
     -->
-
     <TheApp></TheApp>
   </div>
 </template>
@@ -20,3 +19,4 @@ export default {
   }
 }
 </script>
+
