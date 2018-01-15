ece829bb5534f5d7928f8229392ab9422bc8ca96
Vinicius Azevedo <viniazvd@gmail.com>
2018-01-15 00:28:02 -0200

    refactoring

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,20 +1,17 @@
 import { details, plans, prices } from '../services'
 import * as types from './mutations-types'
 
-export const setDetails = ({ commit }) => {
-  return details()
-    .then(res => commit(types.SET_DETAILS, res.data.data))
-    .catch(error => Promise.reject(error))
+export const setDetails = async ({ commit }) => {
+  const { data } = await details()
+  commit(types.SET_DETAILS, data)
 }
 
-export const setPlans = ({ commit }) => {
-  return plans()
-    .then(res => commit(types.SET_PLANS, res.data.data))
-    .catch(error => Promise.reject(error))
+export const setPlans = async ({ commit }) => {
+  const { data } = await plans()
+  commit(types.SET_PLANS, data)
 }
 
-export const setPrices = ({ commit }) => {
-  return prices()
-    .then(res => commit(types.SET_PRICES, res.data.data))
-    .catch(error => Promise.reject(error))
+export const setPrices = async ({ commit }) => {
+  const { data } = await prices()
+  commit(types.SET_PRICES, data)
 }
