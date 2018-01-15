c23e92ad689afceeb158b59c1096a5abff806cee
Vinicius Azevedo <viniazvd@gmail.com>
2018-01-15 00:28:55 -0200

    add validateStatus and interceptor

--- src/support/http/index.js
+++ src/support/http/index.js
@@ -1,6 +1,22 @@
 import axios from 'axios'
 import { BASE_URL } from '../enviroment/baseUrl'
 
-export default axios.create({
-  baseURL: BASE_URL
+const instance = axios.create({
+  baseURL: BASE_URL,
+
+  validateStatus (status) {
+    return status >= 200 && status < 300
+  }
 })
+
+export default instance
+
+instance.interceptors.response.use(mergeRedundantData)
+
+function mergeRedundantData (response) {
+  if (response.data && response.data.data) {
+    return Object.assign(response, response.data)
+  }
+
+  return response
+}
