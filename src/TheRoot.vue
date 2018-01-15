e12a4a7c7912adba5c7b5c96aed94393badc4c18
Vinicius Azevedo <viniazvd@gmail.com>
2018-01-15 16:02:54 -0200

    add loader

--- src/TheRoot.vue
+++ src/TheRoot.vue
@@ -4,6 +4,14 @@
       Segundo o style-guide oficial do vue, os componentes que só devem ter uma única
      instância ativa devem começar com o Theprefixo, para denotar que pode haver apenas um. 
     -->
+
+    <Loader 
+      :loading="this.showLoader" 
+      size="30px"
+      color="white"
+      class="loader--configuration">
+    </Loader>
+
     <TheApp></TheApp>
   </div>
 </template>
@@ -15,8 +23,27 @@ export default {
 
   // lazy loading...
   components: {
-    TheApp: () => import('./components/TheApp')
+    TheApp: () => import('./app/speakMore/components/TheApp'),
+    Loader: () => import('vue-spinner/src/PulseLoader.vue')
+  },
+
+  computed: {
+    showLoader () {
+      return this.$store.state.loader.loading
+    }
   }
 }
 </script>
 
+<style lang="sass" scoped>
+@import "./assets/sass/_base.sass"; 
+
+.loader--configuration
+  background-color: $color__primary--light;
+  position: absolute;
+  z-index: 99999;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  padding-top: 350px;
+</style>
