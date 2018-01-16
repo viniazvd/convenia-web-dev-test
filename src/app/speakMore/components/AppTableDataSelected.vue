76d87a47ba189305a81fa9c8a494d1b51481c08d
Vinicius Azevedo <viniazvd@gmail.com>
2018-01-16 03:11:52 -0200

    using divs instead of table + responsive

--- src/app/speakMore/components/AppTableDataSelected.vue
+++ src/app/speakMore/components/AppTableDataSelected.vue
@@ -1,23 +1,18 @@
 <template>
   <transition name="bounce">
-    <div v-if="this.wasChose()">
-      <table>
-        <thead>
-          <tr>
-            <th>Origem</th>
-            <th>Destino</th>
-            <th>Tempo da ligação</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{{ this.getNameStateOrigin }}</td>
-            <td>{{ this.getNameStateDestiny }}</td>
-            <td>{{ this.getTime }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+      <section class="container" v-if="this.wasChose()">
+        <div class="container__labels">
+          <div class="item">Origem</div>
+          <div class="item">Destino</div>
+          <div class="item">Tempo da ligação</div>
+        </div>
+
+        <div class="container__data">
+          <div class="item">{{ this.getNameStateOrigin }}</div>
+          <div class="item">{{ this.getNameStateDestiny }}</div>
+          <div class="item">{{ this.getTime }}</div>
+        </div>
+      </section>
   </transition>
 </template>
 
@@ -61,7 +56,39 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-@import "../../../assets/sass/table.sass"; 
 @import "../../../assets/sass/transition.sass"; 
-</style>
+@import "../../../assets/sass/_base.sass"; 
 
+@mixin defaultContainer()
+  display: flex;
+  flex-direction: column;
+  width: 75%;
+  border: 1px solid $color__primary--light;
+  padding-top: 15px;
+  padding-bottom: 15px;
+
+.container 
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+
+.container__labels
+  @include defaultContainer()
+  color: $color__white;
+  background: $color__primary--light;
+
+.container__data
+  @include defaultContainer()
+
+.item 
+  flex: 1 1 auto;
+  width: 99%;
+
+@media screen and(min-width: 500px)
+  .container__labels
+    flex-direction: row;
+
+  .container__data
+    flex-direction: row;
+</style>
--- src/app/speakMore/components/AppTablePromotions.vue
+++ src/app/speakMore/components/AppTablePromotions.vue
@@ -1,26 +1,25 @@
 <template>
-  <table>
-    <thead>
-      <tr>
-        <th v-for="plan in this.plans" :key="plan.plan_id"> 
-        {{ plan.plan_name }}
-        </th>
-        <th>
-          Normal
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td v-for="value in this.withPromotion" :key="value.uniqueKeyNotFound">
-          {{ value }}
-        </td>
-        <td>
-          {{ this.withoutPromotion }} 
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <section class="container">
+    <div class="container__plans">
+      <div 
+        class="item"
+        v-for="plan in this.plans" 
+        :key="plan.plan_id">
+      {{ plan.plan_name }}  
+      </div>
+      <div class="item">Normal</div>
+    </div>
+
+    <div class="container__prices">
+      <div 
+        class="item"
+        v-for="value in this.withPromotion" 
+        :key="value.uniqueKeyNotFound">
+      {{ value }}  
+      </div>
+      <div class="item">{{ this.withoutPromotion }}</div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -32,6 +31,42 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-@import "../../../assets/sass/table.sass"; 
+@import "../../../assets/sass/_base.sass"; 
+
+.container 
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+
+.container__plans
+  display: flex;
+  flex-direction: column;
+  background: $color__primary--light;
+  color: $color__white;
+  width: 75%;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  border: 1px solid $color__primary--light;
+
+.container__prices
+  display: flex;
+  flex-direction: column;
+  background: $color__white;
+  width: 75%;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  border: 1px solid $color__primary--light;
+
+.item 
+  flex: 1 1 auto;
+  width: 99%;
+
+@media screen and(min-width: 500px)
+  .container__plans
+    flex-direction: row;
+
+  .container__prices
+    flex-direction: row;
 </style>
 
